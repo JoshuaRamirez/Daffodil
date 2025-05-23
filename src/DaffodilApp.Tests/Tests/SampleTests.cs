d1ff89d754a1f28fa03e5ef4a6dd0cd84b98d3a9
Joshua Ramirez <joshua.ramirez@me.com>
2025-05-23 16:21:11 +0700

    Add .NET setup with sample app and tests

---
+++ src/DaffodilApp.Tests/Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DaffodilApp.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void TestHello()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
